3a6393b46aeca40133d01fc752081a5bc34b52aa
YASH <supermenant@gmail.com>
2024-11-29 22:46:13 +0530

    Add files via upload

---
+++ newprogram.py
@@ -0,0 +1,31 @@
+#STRING
+str1="P"
+str2="R"
+str3="R"
+print(str1,str2,str3)
+#Concatenation
+str4="apna"
+str5="kv"
+final=(str4+str5)
+print(final)
+#len fuction
+len1=len(str4)
+print(len1)
+dinal=str4+" "+str5
+print(dinal)
+#indexin
+str="KHANA PARA"
+print(str[4],str[5])
+#slicing
+print([4:7])
+print([4: ])
+print([ :6])
+print([1:len(str)]#same as [4: ]
+print(-3:-1)
+#endswith function
+print(str.endswith("ARA"))
+
+
+
+
+
